f33a9a849a69f9989390e17ea0afd754bce47c00
Evi <69012052+Evi-2003@users.noreply.github.com>
2023-01-12 00:19:07 +0100

    1.0

---
+++ maps.js
@@ -0,0 +1,228 @@
+var map;
+var directionsService = new google.maps.DirectionsService();
+var directionsRenderer = new google.maps.DirectionsRenderer();
+const { BICYCLING, TRANSIT } = google.maps.DirectionsTravelMode;
+
+function initMap() {
+    // Create a new map
+    map = new google.maps.Map(document.getElementById("googleMaps"), {
+        center: { lat: 52.3547418, lng: 4.8215607},
+        zoom: 13,
+        styles: 
+        [
+            {
+                "featureType": "landscape",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#c9edc5"
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.man_made",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#f4f3f0"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural.terrain",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#bcdfb8"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#c8eec4"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#c9edc5"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.sports_complex",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#c8eec4"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "lightness": 100
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "weight": "1"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "lightness": "0"
+                    },
+                    {
+                        "color": "#ffe492"
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    },
+                    {
+                        "weight": "2.53"
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#ffffff"
+                    },
+                    {
+                        "weight": "0.50"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "lightness": 700
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#7dcdcd"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#abd9ee"
+                    }
+                ]
+                
+            }
+        ]
+    });
+    directionsRenderer.setMap(map);
+    calcRoute();
+}
+
+function calcRoute() {
+    var request = {
+        origin: "Tourniairestraat 1",
+        destination: "Wibautstraat 1",
+        travelMode: BICYCLING,
+        waypoints: [{
+            location: "Osdorpplein 126, 1068 EN Amsterdam",
+            stopover: true
+        },
+        {
+            location: "Rijksmuseum",
+            stopover: true
+        }]
+    };
+
+    // Pass the request to the directions service
+    directionsService.route(request, function (result, status) {
+        if (status == "OK") {
+            // Update the directions renderer with the new route
+            directionsRenderer.setDirections(result);
+        }
+    });
+}
+
+window.onload = initMap;
+
